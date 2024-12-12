6ec6513672e443d7750da67e3458d7cb0186f841
Tran tuan <trantuan9122003@gmail.com>
2024-12-12 19:55:23 +0700

    upload file add_to_cart

---
+++ add_to_cart.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+?>
+
+<?php
+// Bước 1: Kiểm tra xem có sản phẩm ID được gửi từ trang index.php hay không
+if (isset($_POST['productId'])) {
+   $productId = $_POST['productId'];
+
+   // Bước 2: Kiểm tra xem có giỏ hàng trong phiên hiện tại hay không
+   if (!isset($_SESSION['cart'])) {
+      $_SESSION['cart'] = array();
+   }
+
+   // Bước 3: Kiểm tra xem sản phẩm đã tồn tại trong giỏ hàng chưa
+   if (isset($_SESSION['cart'][$productID])) { // Lỗi: Sử dụng biến sai tên ($productID)
+      // Nếu sản phẩm đã tồn tại, tăng số lượng lên 1
+      $_SESSION['cart'][$productID]++; // Lỗi: Sử dụng biến sai tên ($productID)
+   } else {
+      // Nếu sản phẩm chưa tồn tại, thêm vào giỏ hàng với số lượng là 1
+      $_SESSION['cart'][$productID] = 1; // Lỗi: Sử dụng biến sai tên ($productID)
+   }
+
+   // Bước 4: Trả về phản hồi thành công
+   echo "success";
+}
+?>
